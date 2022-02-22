69a4147001451873d658d599d4bc30ca3ddc69b5
michalrys <michalrys@gmail.com>
2022-02-22 23:58:20 +0100

    bugfix for cascade: remove redunant previe method. BUGFIX - when user is removed -> photos which he likes were also removed :(

--- src/main/java/pl/edu/agh/mwo/hibernate/Main.java
+++ src/main/java/pl/edu/agh/mwo/hibernate/Main.java
@@ -22,7 +22,7 @@ public class Main {
         new DataBaseReseter().rebuildDatabase();  // USE THIS TO REBUILD DATABASE
         main.logger.write("! Database was rebuilt to original state.\n");
         // STEP 1: print db initial data - in order to have it, run database_setup.sql in SQLite
-//        main.printEntitiesForCheck();
+        main.previewDataBase();
 
         // STEP 2: point 4.1. Adding new data to db
         main.previewDataBase();
@@ -51,52 +51,6 @@ public class Main {
 
 
         main.close();
-    }
-
-    private void printEntitiesForCheck() {
-        Query<User> query = session.createQuery("from User", User.class);
-        List<User> users = query.list();
-        System.out.println(users);
-        for (User user : users) {
-            Set<Album> albums = user.getAlbums();
-            System.out.println(user.getName());
-            System.out.println("\tAlbums:");
-            for (Album album : albums) {
-                System.out.println("\t\t" + album.getName());
-            }
-            System.out.println("\tLiked photos:");
-            for (Photo photo : user.getLikedPhotos()) {
-                System.out.println("\t\t" + photo.getName());
-            }
-            System.out.println("----------");
-        }
-
-        System.out.println("== photo ==");
-        Query<Photo> from_photo = session.createQuery("from Photo", Photo.class);
-        List<Photo> photos = from_photo.list();
-        System.out.println(photos);
-
-        System.out.println("== Albums ==");
-        Query<Album> from_album = session.createQuery("from Album", Album.class);
-        List<Album> albums = from_album.list();
-        System.out.println(albums);
-        for (Album album : albums) {
-            System.out.println("\tAlbum: ");
-            System.out.println("\t\t" + album);
-            System.out.println("\tPhotos:");
-            for (Photo photo : album.getPhotos()) {
-                System.out.println("\t\t" + photo.getName());
-                Set<User> likesByUsers = photo.getUsersWhoLikedPhoto();
-                if (likesByUsers.size() != 0) {
-                    System.out.println("\t\t\tlikes by:");
-                    for (User user : likesByUsers) {
-                        System.out.println("\t\t\t\t" + user.getName());
-                    }
-                } else {
-                    System.out.println("\t\t\t\tnobody likes it.");
-                }
-            }
-        }
     }
 
     private void previewDataBase() {
